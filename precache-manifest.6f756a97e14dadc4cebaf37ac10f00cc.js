5feea0642beaac9cc7adc6a91facbca87b2b1c29
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-09-29 21:54:26 -0500

    Updates

---
+++ precache-manifest.6f756a97e14dadc4cebaf37ac10f00cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e1aa4170510a0097bc1b53e777a94e",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "97360f641056d59144b0",
+    "url": "/react_portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6918ca1f919ff329a428",
+    "url": "/react_portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "97360f641056d59144b0",
+    "url": "/react_portfolio/static/js/2.4931e31f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.4931e31f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6918ca1f919ff329a428",
+    "url": "/react_portfolio/static/js/main.83cccfa7.chunk.js"
+  },
+  {
+    "revision": "40662b00510a68be1fe5",
+    "url": "/react_portfolio/static/js/runtime-main.39dc65c8.js"
+  }
+]);
